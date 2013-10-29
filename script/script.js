ce7b3c0b612796ebd4c6f5d4b706e95c188a2c82
Acsa Lu <acsa0210@gmail.com>
2013-10-29 21:51:53 +0800

    select pin

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 var map;
+boolean canSelectPin = true;
+
 function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(23.597, 161.644),
@@ -60,13 +62,13 @@ function updateNode(node, parms) {
 $(document).ready(function() {
     Parse.initialize("9viWGl0x5YuJqhwg8dbYG16snlA2WG5X26JJrI7d", "u99KHyIjasLOmTlfePq01sk3k1ZqsRUyHlhmkaS3");
     
-    
     $('#place-pin').click(function() {
         console.log("place-pin");
     });
 
     $('#select-pin').click(function() {
         console.log("select-pin");
+
     });
 
     $('#unselect-pin').click(function() {
