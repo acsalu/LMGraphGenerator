558b6cec243bb1dc031d9ec412553baf65ffd01b
Acsa Lu <acsa0210@gmail.com>
2013-10-29 21:47:50 +0800

    merge code

--- script/script.js
+++ script/script.js
@@ -1,81 +1,81 @@
 var map;
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(23.597, 161.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
+function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(23.597, 161.644),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+        mapOptions);
 
-        google.maps.event.addListener(map, 'click', function(event) {
-                placeMarker(event.latLng );
-                createNode( event.latLng.lat(),event.latLng.lng() );
-        });
+    google.maps.event.addListener(map, 'click', function(event) {
+            placeMarker(event.latLng );
+            createNode( event.latLng.lat(),event.latLng.lng() );
+    });
+}
 
-      }
+
+function placeMarker(location) {
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+}
 
 
-      function placeMarker(location) {
-        var marker = new google.maps.Marker({
-          position: location,
-          map: map
-        });
+function createNode(lat, lng) {
+    console.log("create-node");
 
-      }
+    var Node = Parse.Object.extend("Node");
+    var node = new Node();
+    node.save({lat:lat, lng:lng}, {
+        success: function(object) {
+            console.log("Node(" + lat + ", " + lng + ") saved!");
+          }
+    });
+
+    return node;
+}
+
+function createEdge(node1, node2) {
+    console.log("create-edge");
+
+    var Edge = Parse.Object.extend("Edge");
+    var edge = new Edge();
+    edge.save({node1:node1, node2:node2}), {
+           success: function(object) {
+            
+        }
+    }
+
+
+}
+
+function updateNode(node, parms) {
+
+}
 
 
 
 $(document).ready(function() {
-	Parse.initialize("9viWGl0x5YuJqhwg8dbYG16snlA2WG5X26JJrI7d", "u99KHyIjasLOmTlfePq01sk3k1ZqsRUyHlhmkaS3");
-	
+    Parse.initialize("9viWGl0x5YuJqhwg8dbYG16snlA2WG5X26JJrI7d", "u99KHyIjasLOmTlfePq01sk3k1ZqsRUyHlhmkaS3");
     
-	$('#place-pin').click(function() {
-		console.log("place-pin");
-	});
+    
+    $('#place-pin').click(function() {
+        console.log("place-pin");
+    });
 
-	$('#select-pin').click(function() {
-		console.log("select-pin");
-	});
+    $('#select-pin').click(function() {
+        console.log("select-pin");
+    });
 
-	$('#unselect-pin').click(function() {
-		console.log("unselect-pin");
-	});
+    $('#unselect-pin').click(function() {
+        console.log("unselect-pin");
+    });
 
-	$('#create-node').click(function() {
-		createNode(23, 120);
-	});
+    $('#create-node').click(function() {
+        createNode(23, 120);
+    });
 
-	function createNode(lat, lng) {
-		console.log("create-node");
-
-		var Node = Parse.Object.extend("Node");
-		var node = new Node();
-		node.save({lat:lat, lng:lng}, {
-			success: function(object) {
-		    	console.log("Node(" + lat + ", " + lng + ") saved!");
-		  	}
-		});
-
-		return node;
-	}
-
-	function createEdge(node1, node2) {
-		console.log("create-edge");
-
-		var Edge = Parse.Object.extend("Edge");
-		var edge = new Edge();
-		edge.save({node1:node1, node2:node2}), {
-	   		success: function(object) {
-                
-			}
-		}
-
-
-	}
-
-	function updateNode(node, parms) {
-
-	}
 
 });
